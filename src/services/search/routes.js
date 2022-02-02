1d9f3741544a4f7bcac4887242a7f337d789f7d2
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-02 19:17:24 +0100

    returning 401

--- src/services/search/routes.js
+++ src/services/search/routes.js
@@ -1,11 +1,21 @@
-import express from "express";
+import express, { response } from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 // Get the weather depensing on the city (5 differet cities)
-// router.route("/").get(async (req, res, next) =>{ })
-router.route("/").get((req, res, next) => {
-    res.status(200).send("Hello World!");
+router.route("/").get(async (req, res, next) => {
+  try {
+    let results = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=London&appid=${process.env.API_KEY}`
+    );
+    if (res.ok) {
+      res.send(results);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
-export default router
+export default router;
