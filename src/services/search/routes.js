5dd8e8379040b0a85c193872d018634a61b43b72
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-03 20:53:13 +0100

    added yelp api key

--- src/services/search/routes.js
+++ src/services/search/routes.js
@@ -3,13 +3,13 @@ import axios from "axios";
 import "dotenv/config";
 
 const router = express();
+// WEATHER_APP_BASE_URL = https://api.openweathermap.org/data/2.5/weather?q=
 
 router.get("/", async (req, res, next) => {
   try {
     let response = await axios.get(
-      `${process.env.WEATHER_APP_BASE_URL}London&appid=${process.env.API_KEY}`
+      `${process.env.WEATHER_APP_BASE_URL}London&appid=${process.env.OPEN_WEATHER_API_KEY}`
     );
-    // console.log(response);
     res.status(200).send(response.data);
   } catch (error) {
     console.log(error);
