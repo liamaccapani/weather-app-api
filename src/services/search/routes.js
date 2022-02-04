4b5a452f03dd092fe7523c37499653174e619308
Lia Maccapani <lia.maccapani@gmail.com>
2022-02-04 11:05:53 +0100

    getting weather and yelp for London

--- src/services/search/routes.js
+++ src/services/search/routes.js
@@ -11,34 +11,35 @@ router.get("/", async (req, res, next) => {
   const edinburgh = "Edinburgh";
   const tokyo = "Tokyo";
   try {
-    let weather = await axios.get(
+    // LONDON
+    let weatherResponse = await axios.get(
       `${process.env.WEATHER_APP_BASE_URL}${london}&appid=${process.env.OPEN_WEATHER_API_KEY}`
     );
-    // The response object you get from the open weather API is circular type(objects which reference themselves). 
-    // JSON.stringify will throw an error when it comes through a circular reference. 
+
+    let yelpResponse = await axios.get(
+      `${process.env.YELP_BASE_URL}location=${london}&limit=5&sort_by=rating`,
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.YELP_API_KEY
+        },
+      }
+    );
+    // The response object you get from the open weather API is circular type(objects which reference themselves).
+    // JSON.stringify will throw an error when it comes through a circular reference.
     // This is the reason you are getting this error while using send method.
     // That's why send(response.data)
-    let weatherResponse = weather.data
-    res.status(200).send(weatherResponse);
+    let weatherLondon = weatherResponse.data;
+    let yelpLondon = yelpResponse.data;
+
+    const returnValues = {...weatherLondon, ...yelpLondon}; 
+
+    res.status(200).send(returnValues);
   } catch (error) {
     console.log(error);
     next(error);
   }
 });
 
-
 // https://api.yelp.com/v3/businesses/search?location=London&limit=5&sort_by=rating
-
-// router.get("/", async (req, res, next) => {
-//   try {
-//     let response = await axios.get(
-//       `${process.env.WEATHER_APP_BASE_URL}London&appid=${process.env.OPEN_WEATHER_API_KEY}`
-//     );
-//     res.status(200).send(response.data);
-//   } catch (error) {
-//     console.log(error);
-//     next(error);
-//   }
-// });
 
 export default router;
